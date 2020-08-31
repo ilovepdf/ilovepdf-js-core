8f671666415d3a87d2edb6f9b8afd76e5e3e29bc
Diego Lao Tebar <diego.lao@ilovepdf.com>
2020-08-31 11:59:00 +0200

    Fix expect from getSignatureTemplate method and added new test using templates in batch mode

--- src/tasks/sign/SignTask.test.ts
+++ src/tasks/sign/SignTask.test.ts
@@ -194,7 +194,7 @@ describe('SignTask', () => {
             return ILovePDFCoreApi.getSignatureTemplate(auth, xhr, task);
         })
         .then(data => {
-            expect(data.name).toBe('This is my template');
+            expect(data.template_name).toBe('This is my template');
         });
     });
 
@@ -246,8 +246,67 @@ describe('SignTask', () => {
             const signTaskFromTemplate = taskFactory.newTask('sign', auth, xhr) as SignTask;
             await signTaskFromTemplate.start();
 
-            const templateElement = JSON.parse(template.elements) as TemplateElement;
-            return signTaskFromTemplate.processFromTemplate(templateElement);
+            return signTaskFromTemplate.processFromTemplate(template);
+        })
+        .then(signTaskFromTemplate => {
+            expect(signTaskFromTemplate.responses.process[0].signers[0].name).toBe('Diego Signer');
+        });
+    });
+
+    it('process in batch mode from template', () => {
+        const task = taskFactory.newTask('sign', auth, xhr) as SignTask;
+
+        return task.start()
+        .then(() => {
+            return task.addFile('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf');
+        })
+        .then(() => {
+            return task.addFile('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf');
+        })
+        .then(() => {
+            // Requester.
+            task.requester = {
+                name: 'Diego',
+                email: 'emailto@test.com'
+            };
+
+            // Signer.
+            const file = task.getFiles()[0];
+            const signatureFile = new SignatureFile(file, [{
+                type: 'signature',
+                position: '300 -100',
+                pages: '1',
+                size: 28,
+                color: '#000000',
+                font: null as unknown as string,
+                content: null as unknown as string
+            }]);
+
+            const signer = new Signer('Diego', 'emailto@test.com', {
+                type: 'signer',
+                force_signature_type: 'all'
+            });
+            task.addSigner(signer);
+
+            return task.saveAsTemplate({
+                template_name: 'Plantillita',
+                mode: 'batch',
+                batch_elements: [ signatureFile ],
+                custom_int: 0,
+                custom_string: '0'
+            });
+        })
+        .then(() => {
+            const xhr = new XHRPromise();
+            const auth = new JWT(xhr, process.env.PUBLIC_KEY!, process.env.SECRET_KEY!);
+
+            return ILovePDFCoreApi.getSignatureTemplate(auth, xhr, task.id);
+        })
+        .then(async (template) => {
+            const signTaskFromTemplate = taskFactory.newTask('sign', auth, xhr) as SignTask;
+            await signTaskFromTemplate.start();
+
+            return signTaskFromTemplate.processFromTemplate(template);
         })
         .then(signTaskFromTemplate => {
             expect(signTaskFromTemplate.responses.process[0].signers[0].name).toBe('Diego Signer');
@@ -446,6 +505,5 @@ describe('SignTask', () => {
             expect(status.document).toBe('sent');
         });
     });
-
 
 });
