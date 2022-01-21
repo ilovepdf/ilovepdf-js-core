fb31be168ec8b25da1f7c0f67737a1061871f2bd
Diego Lao Tebar <diego.lao@ilovepdf.com>
2022-01-21 12:20:31 +0100

    Fix removed class that was not substituted

--- src/types/responses/GetSignatureResponse.ts
+++ src/types/responses/GetSignatureResponse.ts
@@ -1,5 +1,53 @@
-import SignatureProcessResponse from "./SignatureProcessResponse";
+import GetSignerResponse from "./GetSignerResponse";
+import SignatureStatus from "./SignatureStatus";
+import ServerFile from "../ServerFile";
 
-type GetSignatureResponse = SignatureProcessResponse;
+type GetSignatureResponse = {
+    /**
+     * Task id.
+     */
+    task: string;
+    /**
+     * Requester name.
+     */
+    name: string;
+    /**
+     * Requester email.
+     */
+    email: string;
+    /**
+     * Stringified creation date.
+     */
+    created: string;
+    /**
+     * Stringified expiration date.
+     */
+    expires: string;
+    /**
+     * Attribute that works like a signature id in order
+     * to manage the signature after task is processed.
+     */
+    token_requester: string;
+    /**
+     * Signature status.
+     */
+    status: SignatureStatus;
+    /**
+     * Signature signers
+     */
+    signers: Array<GetSignerResponse>;
+    /**
+     * Custom number to filter in the future.
+     */
+    custom_int: number;
+    /**
+     * Custom string to filter in the future.
+     */
+    custom_string: string;
+    /**
+     * Signature files.
+     */
+    files: Array<ServerFile>;
+};
 
 export default GetSignatureResponse;
