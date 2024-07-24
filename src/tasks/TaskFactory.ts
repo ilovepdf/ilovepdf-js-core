6dce2513c8b751aab9497470f518f13761287681
marc_r <marc.rodriguez@ilovepdf.com>
2024-07-24 12:14:27 +0200

    chore: typecheck all tools are in TaskFactory

--- src/tasks/TaskFactory.ts
+++ src/tasks/TaskFactory.ts
@@ -1,5 +1,5 @@
 import ILovePDFTool from "../types/ILovePDFTool";
-import { TaskParams } from "./Task";
+import Task, { TaskParams } from "./Task";
 import MergeTask from "./MergeTask";
 import TaskTypeNotExistsError from "../errors/TaskTypeNotExistsError";
 import SplitTask from "./SplitTask";
@@ -27,6 +27,27 @@ export interface TaskFactoryI {
     newTask: (taskType: ILovePDFTool, auth: Auth, xhr: XHRInterface, params?: TaskParams) => TaskI;
 }
 
+const tasks: {[key in ILovePDFTool]: typeof Task} = {
+    merge: MergeTask,
+    split: SplitTask,
+    compress: CompressTask,
+    extract: ExtractTask,
+    htmlpdf: HtmlPdfTask,
+    imagepdf: ImagePdfTask,
+    officepdf: OfficePdfTask,
+    pagenumber: PageNumberTask,
+    pdfa: PdfaTask,
+    pdfjpg: PdfJpgTask,
+    protect: ProtectTask,
+    repair: RepairTask,
+    rotate: RotateTask,
+    unlock: UnlockTask,
+    validatepdfa: ValidatePdfaTask,
+    watermark: WatermarkTask,
+    sign: SignTask,
+    editpdf: EditTask,
+};
+
 export default class TaskFactory implements TaskFactoryI {
 
     /**
@@ -35,63 +56,12 @@ export default class TaskFactory implements TaskFactoryI {
      * @param params - Parameters to customize the process.
      */
     newTask(taskType: ILovePDFTool, auth: Auth, xhr: XHRInterface, params: TaskParams = {}): TaskI {
-        if (taskType === 'merge') {
-            return new MergeTask(auth, xhr, params);
+        const task: any = tasks[taskType];
+        if (typeof task === 'undefined') {
+            // Don't return and throw an error.
+            throw new TaskTypeNotExistsError();
         }
-        else if (taskType === 'split') {
-            return new SplitTask(auth, xhr, params);
-        }
-        else if (taskType === 'compress') {
-            return new CompressTask(auth, xhr, params);
-        }
-        else if (taskType === 'extract') {
-            return new ExtractTask(auth, xhr, params);
-        }
-        else if (taskType === 'htmlpdf') {
-            return new HtmlPdfTask(auth, xhr, params);
-        }
-        else if (taskType === 'imagepdf') {
-            return new ImagePdfTask(auth, xhr, params);
-        }
-        else if (taskType === 'officepdf') {
-            return new OfficePdfTask(auth, xhr, params);
-        }
-        else if (taskType === 'pagenumber') {
-            return new PageNumberTask(auth, xhr, params);
-        }
-        else if (taskType === 'pdfa') {
-            return new PdfaTask(auth, xhr, params);
-        }
-        else if (taskType === 'pdfjpg') {
-            return new PdfJpgTask(auth, xhr, params);
-        }
-        else if (taskType === 'protect') {
-            return new ProtectTask(auth , xhr, params);
-        }
-        else if (taskType === 'repair') {
-            return new RepairTask(auth, xhr, params);
-        }
-        else if (taskType === 'rotate') {
-            return new RotateTask(auth, xhr, params);
-        }
-        else if (taskType === 'unlock') {
-            return new UnlockTask(auth, xhr, params);
-        }
-        else if (taskType === 'validatepdfa') {
-            return new ValidatePdfaTask(auth, xhr, params);
-        }
-        else if (taskType === 'watermark') {
-            return new WatermarkTask(auth, xhr, params);
-        }
-        else if (taskType === 'sign') {
-            return new SignTask(auth, xhr, params);
-        }
-        else if (taskType === 'editpdf') {
-            return new EditTask(auth, xhr, params);
-        }
-
-        // Don't return and throw an error.
-        throw new TaskTypeNotExistsError();
+        return new task(auth, xhr, params);
     }
 
 }
