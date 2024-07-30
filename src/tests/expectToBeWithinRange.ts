d6a71f5fe8e7dcb84cbac5ddce5a6862c8139957
marc_r <marc.rodriguez@ilovepdf.com>
2024-07-30 12:24:31 +0200

    test: implement expect.toBeWithinRange matcher

---
+++ src/tests/expectToBeWithinRange.ts
@@ -0,0 +1,34 @@
+import {expect} from "@jest/globals";
+
+export const expectInRange = (actual: number, expected: number, errorMargin: number) => {
+    expect(actual).toBeGreaterThan(expected - errorMargin)
+    expect(actual).toBeLessThan(expected + errorMargin)
+}
+
+expect.extend({
+    toBeWithinRange(actual: number, expected: number, errorMargin: number) {
+        const { isNot } = this
+        const printExpected = this.utils.printExpected
+        const floor = expected - errorMargin
+        const ceiling = expected + errorMargin
+        const pass = actual >= floor && actual <= ceiling;
+        return {
+            pass,
+            message: () =>
+                `expected ${this.utils.printReceived(
+                    actual,
+                )} ${isNot ? 'not ' : ''}to be within range ${printExpected(
+                    `${floor} - ${ceiling}`,
+                )} (expected: ${printExpected(expected)}, errorMargin: ${printExpected(errorMargin)})`,
+        }
+    },
+});
+
+declare module 'expect' {
+    interface AsymmetricMatchers {
+        toBeWithinRange(expected: number, errorMargin: number): void;
+    }
+    interface Matchers<R> {
+        toBeWithinRange(expected: number, errorMargin: number): R;
+    }
+}
